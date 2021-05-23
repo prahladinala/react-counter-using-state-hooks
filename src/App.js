ef39bf3a368c7c29bb37ab9160f7fac7bcfa56bb
prahladinala <prahlad2018@gmail.com>
2021-05-23 18:58:56 +0530

    Basic Counter UI Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react"
+import "./App.css"
 
 function App() {
+  // const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(true)
+  // const [count, setCount] = useState(false)
+  // const [count, setCount] = useState("name")
+  // const [count, setCount] = useState("superman")
+  // const [count, setCount] = useState({
+  //   name: "",
+  //   email: "gmail.com"
+  // })
+
+  const [count, setCount] = useState(0)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>Counter app using state</h1>
       </header>
+      <h2>Current value of count is {count}</h2>
+      <button>Reset Counter</button>
+      <button >Increment Counter</button>
+      <button>Decrement Counter</button>
     </div>
-  );
+  )
 }
-
 export default App;
